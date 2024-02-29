ef4344f8e7baf88df9a4dfa7d1d5ee9006ac49d2
SiRipo92 <ripochesierra@gmail.com>
2024-02-29 17:04:46 +0100

    Ajouter des styles de thème et fichiers pour héberger Google Fonts localement

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -2,12 +2,8 @@
 
 add_action( 'wp_enqueue_scripts', 'chicdressing_enqueue_styles' );
 function chicdressing_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/chicdressing/style.css', array(), filemtime(get_stylesheet_directory() . '/chicdressing/style.css'));
 }
 
 add_filter( 'big_image_size_threshold', '__return_false' );
-
-
-
-
- 
