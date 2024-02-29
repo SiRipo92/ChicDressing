61a63c635e52b68638eb02b0150dfb194ec79fe8
SiRipo92 <ripochesierra@gmail.com>
2024-02-29 17:55:57 +0100

    Ajout de fichiers et différences binaires dans le répertoire wp-content et les thèmes chicdressing.

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -3,7 +3,14 @@
 add_action( 'wp_enqueue_scripts', 'chicdressing_enqueue_styles' );
 function chicdressing_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/chicdressing/style.css', array(), filemtime(get_stylesheet_directory() . '/chicdressing/style.css'));
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
 }
-
+// Réduit la taille des images
 add_filter( 'big_image_size_threshold', '__return_false' );
+
+// Supprime les polices Google de Ashe Theme
+wp_dequeue_style('ashe-playfair-font');
+wp_dequeue_style('ashe-opensans-font');
+wp_dequeue_style('ashe-kalam-font');
+wp_dequeue_style('ashe-rokkitt-font');
+wp_deregister_style('ashe_gfonts_scripts');
